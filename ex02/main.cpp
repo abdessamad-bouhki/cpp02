609b708d9c020993803c70c9b893043013133683
abdessamad <abdessamadbouhki@gmail.com>
2025-12-25 11:51:18 +0100

    sma

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -2,22 +2,23 @@
 
 int main( void )
 {
-    Fixed a (0.94609375f);
-    std::cout << a << std::endl;
-    Fixed const b( Fixed( 21474836.48f ) * Fixed( 21474836.48f ));
-    std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-    std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-    std::cout << a << std::endl;
-    std::cout << ++a << std::endl;
-    std::cout << a << std::endl;
-    std::cout << a++ << std::endl;
-    std::cout << a << std::endl;
-    std::cout << b << std::endl;
-    int j = 21474836.48 * 21474836.48;
-    std::cout << "yo" <<j << std::endl;
-    std::cout << "yo" << b << std::endl;
-    std::cout << Fixed::max( a, b ) << std::endl;
-    std::cout << 1000.1234567 << std::endl;
-
+    // Fixed a (0.94609375f);
+    // std::cout << a << std::endl;
+    // Fixed const b( Fixed( 21474836.48f ) * Fixed( 21474836.48f ));
+    // std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+    // std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+    // std::cout << a << std::endl;
+    // std::cout << ++a << std::endl;
+    // std::cout << a << std::endl;
+    // std::cout << a++ << std::endl;
+    // std::cout << a << std::endl;
+    // std::cout << b << std::endl;
+    // int j = 21474836.48 * 21474836.48;
+    // std::cout << "yo" <<j << std::endl;
+    // std::cout << "yo" << b << std::endl;
+    // std::cout << Fixed::max( a, b ) << std::endl;
+    // std::cout << 1000.1234567 << std::endl;
+    float j = 100004000;
+    std::cout<<j << "\n";
     return 0;
 } 
